c80feca138f4fcbc36c73fd67418bd9f7679177c
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2019-07-04 13:25:05 -0700

    Rename variables

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -2,30 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-const Table = ({data}) =>
+const Table = ({ data }) =>
     !data.length ? (
         <p>Nothing to show</p>
     ) : (
         <div className="column">
             <h2 className="subtitle">
-                Showing <strong>{data.length} items</strong>
+                Showing <strong>{ data.length } items</strong>
             </h2>
             <table className="table is-striped">
                 <thead>
                 <tr>
-                    {Object.entries(data[0]).map(el => <th key={el.id}>{el[0]}</th>)}
+                    { Object.entries(data[0]).map(entry => <th key={entry[1]}>{entry[0]}</th>) }
                 </tr>
                 </thead>
                 <tbody>
                 {data.map(el => (
                     <tr key={el.id}>
-                        {Object.entries(el).map(el => <td key={el.id}>{el[1]}</td>)}
+                        { Object.entries(el).map(entry => <td key={entry[1]}>{el[1]}</td>) }
                     </tr>
                 ))}
                 </tbody>
             </table>
         </div>
     );
+
 Table.propTypes = {
     data: PropTypes.array.isRequired
 };
